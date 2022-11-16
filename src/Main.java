460c8b6a760d76d5244c15ce96cd75986b314cfe
alperen010010 <bircakalperen@gmail.com>
2022-11-16 17:26:17 +0300

    connection test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+import static java.net.InetAddress.getLocalHost;
+
+
 public class Main {
+    static int port = 8080;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        try {
+            InetAddress localhost = getLocalHost();
+            System.out.println(localhost.getHostAddress());
+
+            ServerSocket socket = new ServerSocket(port);
+
+            Socket s = socket.accept();
+
+            System.out.println("connected");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+            while(true) {
+                String line = in.readLine();
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.out.println("No host found");
+        }
+
+
     }
 }
